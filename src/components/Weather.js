3b6f05666c223792823785c31eeb99ccf649ad8c
Dennis Walsh <lawjolla@aol.com>
2018-09-21 11:29:56 -0700

    Last damn fix

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 const WeatherWrapper = styled.div`
-  overflow: hidden;
+  overflow: scroll;
 `;
 
 class Weather extends React.Component {
