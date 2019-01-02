df86828ebdc01a88256cb411753b36f98ae30704
Dennis Walsh <lawjolla@aol.com>
2019-01-01 22:45:39 -0700

    No Dotenv support

--- src/apollo/withApollo.js
+++ src/apollo/withApollo.js
@@ -1,15 +1,15 @@
-import withApollo from "next-with-apollo";
-import { InMemoryCache } from "apollo-cache-inmemory";
-import { ApolloLink, split } from "apollo-link";
-import ApolloClient from "apollo-client";
-import fetch from "node-fetch";
+import withApollo from "next-with-apollo"
+import { InMemoryCache } from "apollo-cache-inmemory"
+import { ApolloLink, split } from "apollo-link"
+import ApolloClient from "apollo-client"
+import fetch from "node-fetch"
 // import { HttpLink } from "apollo-link-http"
-import { createUploadLink } from "apollo-upload-client";
-import { WebSocketLink } from "apollo-link-ws";
-import { setContext } from "apollo-link-context";
-import { onError } from "apollo-link-error";
-import { getMainDefinition } from "apollo-utilities";
-import { withClientState } from "apollo-link-state";
+import { createUploadLink } from "apollo-upload-client"
+import { WebSocketLink } from "apollo-link-ws"
+import { setContext } from "apollo-link-context"
+import { onError } from "apollo-link-error"
+import { getMainDefinition } from "apollo-utilities"
+import { withClientState } from "apollo-link-state"
 
 // import {
 //   getAccessToken,
@@ -20,12 +20,12 @@ import { withClientState } from "apollo-link-state";
 const linkState = withClientState({
   cache,
   resolvers: {}
-});
+})
 
-const cache = new InMemoryCache({});
+const cache = new InMemoryCache({})
 
 export default withApollo(({ headers }) => {
-  const ssrMode = !process.browser;
+  const ssrMode = !process.browser
   // const renewAuth0Token = setContext((request, { renewToken }) => {
   //   if (
   //     !isValidToken(getAccessToken()) &&
@@ -42,37 +42,41 @@ export default withApollo(({ headers }) => {
   //     ? `Bearer ${getAccessToken()}`
   //     : null;
 
-  const Authorization = () => null;
+  const Authorization = () => null
   const httpLink = createUploadLink({
-    uri: process.env.GRAPHQL_ENDPOINT,
+    uri:
+      process.env.GRAPHQL_ENDPOINT ||
+      `https://tjc-prisma.herokuapp.com/tjc/prod`,
     fetch
-  });
+  })
 
   const wsLink =
     !ssrMode &&
     new WebSocketLink({
-      uri: process.env.SUBSCRIPTIONS_ENDPOINT,
+      uri:
+        process.env.SUBSCRIPTIONS_ENDPOINT ||
+        `wss://tjc-prisma.herokuapp.com/tjc/prod`,
       options: {
         reconnect: true,
         connectionParams: Authorization() && { Authorization: Authorization() }
       }
-    });
+    })
 
   const contextLink = setContext(async () => ({
     headers: {
       ...headers,
       Authorization: Authorization() || ``
     }
-  }));
+  }))
 
   const errorLink = onError(({ graphQLErrors, networkError }) => {
     if (graphQLErrors) {
       graphQLErrors.map(err =>
         console.log(`[GraphQL error]: Message: ${err.message}`)
-      );
+      )
     }
-    if (networkError) console.log(`[Network error]: ${networkError}`);
-  });
+    if (networkError) console.log(`[Network error]: ${networkError}`)
+  })
 
   const links = ApolloLink.from([
     // renewAuth0Token,
@@ -80,29 +84,29 @@ export default withApollo(({ headers }) => {
     contextLink,
     linkState,
     httpLink
-  ]);
+  ])
   const link = wsLink
     ? split(
         // split based on operation type
         ({ query }) => {
-          const definition = getMainDefinition(query);
+          const definition = getMainDefinition(query)
           return (
             definition.kind === "OperationDefinition" &&
             definition.operation === "subscription"
-          );
+          )
         },
         wsLink,
         links
       )
-    : links;
+    : links
 
   // const cache = new InMemoryCache({
   //   dataIdFromObject: ({ id, __typename }) =>
   //     id && __typename ? `${__typename}:${id}` : null
   // })
 
-  return new ApolloClient({ link, ssrMode, cache });
-});
+  return new ApolloClient({ link, ssrMode, cache })
+})
 
 // export default withApollo(({ headers }) => {
 //   const ssrMode = !process.browser
